a59d9aa56436a9c81d4bd80b29709dcde5afa073
AslDev006 <asldev006@gmail.com>
2024-03-31 12:57:48 +0500

    first commit

--- models/admin.py
+++ models/admin.py
@@ -1,4 +1,9 @@
 from django.contrib import admin
 from .models import *
 
-admin.site.register([Service_Model, Service_Section_Model, Contact_Model])
+admin.site.register([Service_Model, Service_Section_Model])
+
+
+@admin.register(Contact_Model)
+class ContactAdmin(admin.ModelAdmin):
+    list_display = ['name', 'phone_number', 'called']
--- models/models.py
+++ models/models.py
@@ -25,5 +25,6 @@ class Contact_Model(models.Model):
     name = models.CharField(max_length=150)
     phone_number = models.CharField(max_length=13)
     called = models.CharField(max_length=100, choices=Call_type, default=Call_type.Boglanilmadi)
+
     def __str__(self):
         return self.name
