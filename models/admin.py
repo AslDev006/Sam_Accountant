e302981c1cd18ce73be55ec2bb54c48c724a038f
AslDev006 <asldev006@gmail.com>
2024-03-31 12:59:25 +0500

    Admin.py is done

--- models/admin.py
+++ models/admin.py
@@ -1,8 +1,6 @@
 from django.contrib import admin
 from .models import *
 
-admin.site.register([Service_Model, Service_Section_Model])
-
 
 @admin.register(Contact_Model)
 class ContactAdmin(admin.ModelAdmin):
