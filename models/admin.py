fef84bde8485628142f22abf8db2232ff5ab1a4e
AslDev006 <asldev006@gmail.com>
2024-04-02 07:05:23 +0500

    first commit

--- models/admin.py
+++ models/admin.py
@@ -1,8 +1,5 @@
 from django.contrib import admin
 from .models import *
-from django.contrib.auth.mixins import PermissionRequiredMixin
-from django.views.generic.detail import DetailView
-
 @admin.register(Contact_Model)
 class ContactAdmin(admin.ModelAdmin):
     list_display = ['name', 'phone_number', 'called']
